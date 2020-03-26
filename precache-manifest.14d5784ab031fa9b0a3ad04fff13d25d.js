81e372597946c781cc0425de8bbeeb38de48df2c
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-03-26 21:13:23 +0700

    Updates

---
+++ precache-manifest.14d5784ab031fa9b0a3ad04fff13d25d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed45f1a722a73bfa5527cdc613672392",
+    "url": "/hakita-test/index.html"
+  },
+  {
+    "revision": "49863a3bc08ef6190488",
+    "url": "/hakita-test/static/css/main.2fd7829d.chunk.css"
+  },
+  {
+    "revision": "78ddebbe1e2714dbcdd2",
+    "url": "/hakita-test/static/js/2.560b72a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hakita-test/static/js/2.560b72a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49863a3bc08ef6190488",
+    "url": "/hakita-test/static/js/main.82e9ca9a.chunk.js"
+  },
+  {
+    "revision": "a98a989323b14e964058",
+    "url": "/hakita-test/static/js/runtime-main.5732bc53.js"
+  },
+  {
+    "revision": "ce47c044df892c3562945fb22d691ed5",
+    "url": "/hakita-test/static/media/404notfound.ce47c044.png"
+  },
+  {
+    "revision": "269a99378bb85f9b699971d0a3d30a62",
+    "url": "/hakita-test/static/media/Menyita-surat-KTP-SIM-kecelakaan-lalu-lintas-hakita-770x400.269a9937.jpeg"
+  },
+  {
+    "revision": "523ff9ce48fec74c169b91f9c19a9ded",
+    "url": "/hakita-test/static/media/Penilangan-Tanpa-Surat-Tugas-Polisi-Hakita-770x400.523ff9ce.png"
+  },
+  {
+    "revision": "7407d918106cb337d8a21a9d11d1d10e",
+    "url": "/hakita-test/static/media/contract_ps.7407d918.png"
+  },
+  {
+    "revision": "5baa1ca4e7b9c31ac589f5dd1e3a0756",
+    "url": "/hakita-test/static/media/double-swipe-transaksi-kartu-kredit-ritel-hakita-scaled-770x400.5baa1ca4.jpg"
+  },
+  {
+    "revision": "3d0343fd072f4eaf526aab03ccb07f3c",
+    "url": "/hakita-test/static/media/konvoi-mobil-motor-indonesia-hukum-hakita-scaled-770x400.3d0343fd.jpg"
+  },
+  {
+    "revision": "d82988abb65782e9980e8f55c0087567",
+    "url": "/hakita-test/static/media/perlindungan-belanja-online-penipu-online-hakita-scaled-770x400.d82988ab.jpg"
+  },
+  {
+    "revision": "5afae27f6fa63549045d888045e06a1f",
+    "url": "/hakita-test/static/media/permission_ps.5afae27f.png"
+  },
+  {
+    "revision": "0815829cf51eba9af3440f5c8b22fc08",
+    "url": "/hakita-test/static/media/tukang-parkir-depan-indomaret-hakita-770x400.0815829c.jpeg"
+  }
+]);
